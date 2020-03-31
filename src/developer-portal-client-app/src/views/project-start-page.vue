e39f2676b9b75160f3f9a3c98d87b003f45855f2
Johanna Selin <johanna@codeisking.se>
2020-03-31 12:48:06 +0200

    info areas added

--- src/developer-portal-client-app/src/views/project-start-page.vue
+++ src/developer-portal-client-app/src/views/project-start-page.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
   div.home.project-page.center(v-if="currentProject")
     div(style="height:100%;display:flex;flex-direction:column;")
-      h1.center Nice! Now, what do you want to do?
+      h1 Nice! Now, what do you want to do?
+      p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis vel neque non laoreet. Ut tempor, est sit amet dapibus molestie, purus quam imperdiet tellus, eget gravida sapien nunc vel quam. Proin et nunc augue. Nullam accumsan massa nec mauris ultricies commodo. Morbi quis finibus dolor. Vivamus vestibulum vel enim at lacinia. Aliquam efficitur, orci eu finibus blandit, eros neque porta justo, nec consectetur nisl velit non dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
       .flex-wrapper
 
         .box
@@ -18,6 +19,7 @@
               p Get access to your user's data or let them access their data from other Open Platforms partners.
               .buttons.mt-4
                 router-link.btn.btn-import.btn-small(to="/integrate-user-data") Add application
+      p Suspendisse bibendum arcu a venenatis venenatis. Donec tincidunt justo consectetur rhoncus luctus. Nulla varius hendrerit sapien eget sagittis. Aliquam vehicula enim ac erat pellentesque mattis. Phasellus dignissim augue eget lectus cursus, quis accumsan elit lacinia. Nam blandit at urna sed posuere. Nam venenatis ipsum lacus, nec consequat nunc dictum ac. In non tempus velit. Mauris quis maximus ipsum, sit amet porttitor ex. Etiam sit amet dui consequat, ultricies sem sed, hendrerit elit.
 
 </template>
 
