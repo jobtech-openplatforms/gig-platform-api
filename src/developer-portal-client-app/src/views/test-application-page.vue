fa3ed5ca1e1b3deea4ea9db79e366eca3515fcd2
Calle Hunefalk <calle@roombler.com>
2020-04-01 14:19:00 +0200

    Added TODO's

--- src/developer-portal-client-app/src/views/test-application-page.vue
+++ src/developer-portal-client-app/src/views/test-application-page.vue
@@ -10,12 +10,17 @@
         #[strong Tip!] #[em To test while developing on localhost, consider using a service like] #[a(href="https://ngrok.com/" target="_blank") ngrok] #[em to receive data from Open Platforms to your development machine.]
       h2 Available test functions
       ol
-        li Authenticate user with Open Platforms
-        li Receive user's data from Open Platforms
+        li
+          strong Authenticate user with Open Platforms
+        li
+          strong Receive user's data from Open Platforms
       h2 Authenticate user with Open Platforms
-      p .
+      p.
+        TODO: Write instructions for connecting with a user account through Auth0 to Open Platforms 
+        and approve the connection.
       h2 Receive user's data from Open Platforms
-      p .
+      p.
+        TODO: Add a button to send updated user data to the application connection url.
 </template>
 
 <script lang="ts">
