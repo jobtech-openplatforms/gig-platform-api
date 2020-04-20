788b998bf2334f67288c061cefa20dabd81ef8a7
Calle Hunefalk <calle@roombler.com>
2020-04-20 14:35:00 +0200

    Info page initial update.

--- src/developer-portal-client-app/src/views/info-page.vue
+++ src/developer-portal-client-app/src/views/info-page.vue
@@ -1,9 +1,19 @@
 <template lang="pug">
   div.info-page
     .card
-      h1 TODO
-      p Needs helpful information to get started.
-      p Could look nicer
+      h1 Open Platforms Overview
+      p.
+        Open Platforms is provided for data mobility for gig workers
+        and creating a foundation for innovation in matching platforms
+        and gigs with gig workers.
+      p.
+        The purpose for Open Platforms is to provide the infrastructure
+        for innovation in the gig economy.
+      h2 Two perspectives: Platform and Application
+      p.
+        In Open Platforms, #[strong platforms] provide data for individual #[strong gig workers]
+        through integration with #[strong applications]. In other words, platforms make 
+        data available for each authenticated gig worker to be retrieved by an application.
 
 </template>
 
