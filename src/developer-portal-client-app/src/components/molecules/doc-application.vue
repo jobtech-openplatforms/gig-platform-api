e0525c2864930ad1fbe2e1642c3db7e5909e1f2d
calle@roombler.com <calle@roombler.com>
2020-05-01 13:13:11 +0200

    Docs updated for platform

---
+++ src/developer-portal-client-app/src/components/molecules/doc-application.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  .box
+      h3.color-import Application
+      .frame-wrapper
+        p Get access to your user's data or let them access their data from other Open Platforms partners.
+          router-link.color-import(to="/application-documentation")  Read more
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component({})
+export default class DocApplication extends Vue {
+}
+</script>
