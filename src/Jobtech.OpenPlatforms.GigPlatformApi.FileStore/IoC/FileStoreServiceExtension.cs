7008ac92c4c19b91584a8d33d3d0d965d212c31e
Björn Milton <bjorn@roombler.com>
2020-05-11 14:50:39 +0200

    Rebuilt filestore to use AWS S3 compatible storage (more specifically a minIO instance).
    Change internal API a bit to better reflect the responsability of the various code-parts.

---
+++ src/Jobtech.OpenPlatforms.GigPlatformApi.FileStore/IoC/FileStoreServiceExtension.cs
@@ -0,0 +1,26 @@
+﻿using Jobtech.OpenPlatforms.GigPlatformApi.FileStore.Config;
+using Jobtech.OpenPlatforms.GigPlatformApi.FileStore.Managers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jobtech.OpenPlatforms.GigPlatformApi.FileStore.IoC
+{
+    public static class FileStoreServiceExtension
+    {
+        public static IServiceCollection AddFileStore(this IServiceCollection collection, IConfiguration configuration)
+        {
+            var fileStoreSection = configuration.GetSection("FileStore");
+            collection.Configure<FileStoreConfig>(c =>
+            {
+                c.AccessKey = fileStoreSection.GetValue<string>("AccessKey");
+                c.SecretKey = fileStoreSection.GetValue<string>("SecretKey");
+                c.StorageEndpoint = fileStoreSection.GetValue<string>("StorageEndpoint");
+                c.BucketName = fileStoreSection.GetValue<string>("BucketName");
+            });
+
+            collection.AddTransient<IFileManager, FileManager>();
+
+            return collection;
+        }
+    }
+}
