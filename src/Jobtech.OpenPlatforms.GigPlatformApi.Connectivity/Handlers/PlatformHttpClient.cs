555826834cba5dc247e526e48711bd3326bf95b4
Calle Hunefalk <calle@roombler.com>
2020-05-12 08:53:20 +0200

    Updated logging

--- src/Jobtech.OpenPlatforms.GigPlatformApi.Connectivity/Handlers/PlatformHttpClient.cs
+++ src/Jobtech.OpenPlatforms.GigPlatformApi.Connectivity/Handlers/PlatformHttpClient.cs
@@ -11,127 +11,128 @@ using Newtonsoft.Json;
 
 namespace Jobtech.OpenPlatforms.GigPlatformApi.Connectivity.Handlers
 {
-    public class PlatformHttpClient : IPlatformHttpClient
+  public class PlatformHttpClient : IPlatformHttpClient
+  {
+    private readonly HttpClient _client;
+    private readonly ILogger<PlatformHttpClient> _logger;
+
+    public PlatformHttpClient(HttpClient client, ILogger<PlatformHttpClient> logger)
     {
-        private readonly HttpClient _client;
-        private readonly ILogger<PlatformHttpClient> _logger;
-
-        public PlatformHttpClient(HttpClient client, ILogger<PlatformHttpClient> logger)
-        {
-            _client = client;
-            _logger = logger;
-        }
-
-        public async Task<PlatformDataUserUpdateResult> GetUserDataFromPlatformAsync(UserDataRequest request, string exportDataUri)
-        {
-            var response = await GetUserDataFromPlatformResponseAsync(request, exportDataUri);
-
-            if (!response.IsSuccessStatusCode)
-            {
-                switch (response.StatusCode)
-                {
-                    case HttpStatusCode.NotFound:
-                        throw new UserNotFoundForPlatformException(request.Username, "Platform reported non existant user");
-                    default:
-                        throw new PlatformCommunicationException($"Got unhandled http status code {response.StatusCode} from platform");
-                }
-            }
-
-            var stringResult = await response.Content.ReadAsStringAsync();
-
-            PlatformDataUserUpdateResult result;
-            try 
-            {
-                result = JsonConvert.DeserializeObject<PlatformDataUserUpdateResult>(stringResult);
-                result.RawData = stringResult;
-            } 
-            catch (Exception e)
-            {
-                _logger.LogError("Could not parse data provided by the platform.");
-                throw new MalformedPlatformDataException(stringResult, "Could not parse data");
-            }
-            
-            return result;
-        }
-
-        public async Task<PlatformDataUserTestResult> TestUserDataFromPlatformAsync(UserDataRequest request, string exportDataUri)
-        {
-            var response = await GetUserDataFromPlatformResponseAsync(request, exportDataUri);
-
-            var stringResult = await response.Content.ReadAsStringAsync();
-
-            PlatformDataUserUpdateResult result = null;
-            if (response.IsSuccessStatusCode)
-            {
-                try
-                {
-                    result = JsonConvert.DeserializeObject<PlatformDataUserUpdateResult>(stringResult);
-                } catch (Exception)
-                {
-                    _logger.LogInformation("Could not deserialize repsonse from platform.");
-                } 
-            }
-
-            return new PlatformDataUserTestResult(result,
-                new TestRequest(null, request
-                    )
-                , new TestResponse(
-                    response.Headers.Select(item => string.Format("{0} : {1}", item.Key, string.Join(", ", item.Value))).ToArray()
-                    , response.StatusCode.ToString()
-                    , stringResult
-                ));
-        }
-
-        private async Task<HttpResponseMessage> GetUserDataFromPlatformResponseAsync(UserDataRequest request, string exportDataUri)
-        {
-            _client.BaseAddress = new Uri(exportDataUri);
-
-            // For GETting the user data from the external platform
-            //_client.DefaultRequestHeaders.Add("myGigDataToken", request.MyGigDataToken);
-            //_client.DefaultRequestHeaders.Add("username", request.Username);
-            //_client.DefaultRequestHeaders.Add("requestId", request.RequestId);
-            //var response = await _client.GetAsync("");
-
-            var content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
-            HttpResponseMessage response;
-            try
-            {
-                response = await _client.PostAsync(exportDataUri, content);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Could not communicate with the platform. Will throw.");
-                throw new PlatformCommunicationException("Could not communicate with platform.");
-            }
-
-            return response;
-        }
+      _client = client;
+      _logger = logger;
     }
 
-    public class UserNotFoundForPlatformException : Exception
+    public async Task<PlatformDataUserUpdateResult> GetUserDataFromPlatformAsync(UserDataRequest request, string exportDataUri)
     {
-        public UserNotFoundForPlatformException(string username, string message = null) : base(message)
+      var response = await GetUserDataFromPlatformResponseAsync(request, exportDataUri);
+
+      if (!response.IsSuccessStatusCode)
+      {
+        switch (response.StatusCode)
         {
-            Username = username;
+          case HttpStatusCode.NotFound:
+            throw new UserNotFoundForPlatformException(request.Username, "Platform reported non existant user");
+          default:
+            throw new PlatformCommunicationException($"Got unhandled http status code {response.StatusCode} from platform");
         }
-        public string Username { get; }
+      }
+
+      var stringResult = await response.Content.ReadAsStringAsync();
+
+      PlatformDataUserUpdateResult result;
+      try
+      {
+        result = JsonConvert.DeserializeObject<PlatformDataUserUpdateResult>(stringResult);
+        result.RawData = stringResult;
+      }
+      catch (Exception ex)
+      {
+        _logger.LogError(ex, "Could not parse data provided by the platform.");
+        throw new MalformedPlatformDataException(stringResult, "Could not parse data");
+      }
+
+      return result;
     }
 
-    public class PlatformCommunicationException : Exception
+    public async Task<PlatformDataUserTestResult> TestUserDataFromPlatformAsync(UserDataRequest request, string exportDataUri)
     {
-        public PlatformCommunicationException(string message = null) : base(message)
-        {
+      var response = await GetUserDataFromPlatformResponseAsync(request, exportDataUri);
 
+      var stringResult = await response.Content.ReadAsStringAsync();
+
+      PlatformDataUserUpdateResult result = null;
+      if (response.IsSuccessStatusCode)
+      {
+        try
+        {
+          result = JsonConvert.DeserializeObject<PlatformDataUserUpdateResult>(stringResult);
         }
+        catch (Exception)
+        {
+          _logger.LogInformation("Could not deserialize repsonse from platform.");
+        }
+      }
+
+      return new PlatformDataUserTestResult(result,
+          new TestRequest(null, request
+              )
+          , new TestResponse(
+              response.Headers.Select(item => string.Format("{0} : {1}", item.Key, string.Join(", ", item.Value))).ToArray()
+              , response.StatusCode.ToString()
+              , stringResult
+          ));
     }
 
-    public class MalformedPlatformDataException: Exception
+    private async Task<HttpResponseMessage> GetUserDataFromPlatformResponseAsync(UserDataRequest request, string exportDataUri)
     {
-        public MalformedPlatformDataException(string rawData, string message = null): base(message)
-        {
-            RawData = rawData;
-        }
+      _client.BaseAddress = new Uri(exportDataUri);
 
-        public string RawData { get; }
+      // For GETting the user data from the external platform
+      //_client.DefaultRequestHeaders.Add("myGigDataToken", request.MyGigDataToken);
+      //_client.DefaultRequestHeaders.Add("username", request.Username);
+      //_client.DefaultRequestHeaders.Add("requestId", request.RequestId);
+      //var response = await _client.GetAsync("");
+
+      var content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+      HttpResponseMessage response;
+      try
+      {
+        response = await _client.PostAsync(exportDataUri, content);
+      }
+      catch (Exception e)
+      {
+        _logger.LogError(e, "Could not communicate with the platform. Will throw.");
+        throw new PlatformCommunicationException("Could not communicate with platform.");
+      }
+
+      return response;
     }
+  }
+
+  public class UserNotFoundForPlatformException : Exception
+  {
+    public UserNotFoundForPlatformException(string username, string message = null) : base(message)
+    {
+      Username = username;
+    }
+    public string Username { get; }
+  }
+
+  public class PlatformCommunicationException : Exception
+  {
+    public PlatformCommunicationException(string message = null) : base(message)
+    {
+
+    }
+  }
+
+  public class MalformedPlatformDataException : Exception
+  {
+    public MalformedPlatformDataException(string rawData, string message = null) : base(message)
+    {
+      RawData = rawData;
+    }
+
+    public string RawData { get; }
+  }
 }
--- src/Jobtech.OpenPlatforms.GigPlatformApi.DeveloperPortal/Controllers/MediaController.cs
+++ src/Jobtech.OpenPlatforms.GigPlatformApi.DeveloperPortal/Controllers/MediaController.cs
@@ -10,93 +10,100 @@ using Jobtech.OpenPlatforms.GigPlatformApi.PlatformEngine.Managers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Raven.Client.Documents;
 
 namespace Jobtech.OpenPlatforms.GigPlatformApi.DeveloperPortal.Controllers
 {
-    [Authorize]
-    [ApiController]
-    [Route("api/[controller]")]
-    public class MediaController : ControllerBase
+  [Authorize]
+  [ApiController]
+  [Route("api/[controller]")]
+  public class MediaController : ControllerBase
+  {
+    private IProjectManager _projectManager;
+    private readonly IPlatformAdminUserManager _platformAdminUserManager;
+    private readonly IDocumentStore _documentStore;
+
+    private readonly IFileManager _fileManager;
+    private readonly ILogger<MediaController> _logger;
+
+    public MediaController(IProjectManager projectManager,
+        IPlatformAdminUserManager platformAdminUserManager,
+        IDocumentStore documentStoreHolder,
+        IFileManager fileManager,
+        ILogger<MediaController> logger)
     {
-        private IProjectManager _projectManager;
-        private readonly IPlatformAdminUserManager _platformAdminUserManager;
-        private readonly IDocumentStore _documentStore;
-
-        private readonly IFileManager _fileManager;
-
-        public MediaController(IProjectManager projectManager, IPlatformAdminUserManager platformAdminUserManager, IDocumentStore documentStoreHolder, IFileManager fileManager)
-        {
-            _projectManager = projectManager;
-            _platformAdminUserManager = platformAdminUserManager;
-            _documentStore = documentStoreHolder;
-            _fileManager = fileManager;
-        }
-
-        [HttpPost]
-        [Route("[action]/{projectNamespace:alpha}/{projectId}")]
-        public async Task<IActionResult> Save(IFormFile file, [FromRoute] string projectNamespace, [FromRoute] string projectId, 
-            CancellationToken cancellationToken)
-        {
-            if (!string.Equals(file.ContentType, "image/jpg", StringComparison.OrdinalIgnoreCase) &&
-                !string.Equals(file.ContentType, "image/jpeg", StringComparison.OrdinalIgnoreCase) &&
-                !string.Equals(file.ContentType, "image/pjpeg", StringComparison.OrdinalIgnoreCase) &&
-                !string.Equals(file.ContentType, "image/gif", StringComparison.OrdinalIgnoreCase) &&
-                !string.Equals(file.ContentType, "image/x-png", StringComparison.OrdinalIgnoreCase) &&
-                !string.Equals(file.ContentType, "image/png", StringComparison.OrdinalIgnoreCase))
-            {
-                throw new Exception("Incorrect file type");
-            }
-
-            // Who's logged in?
-            using var session = _documentStore.OpenAsyncSession();
-            var user = await _platformAdminUserManager.GetByUniqueIdentifierAsync(User.Identity.Name, session);
-
-            projectId = $"{projectNamespace}/{projectId}"; 
-
-            var testMode = TestProjectId.IsValidIdentity(projectId) && !ProjectId.IsValidIdentity(projectId);
-
-            // Which project are we working on?
-            var project = testMode ? 
-                await _projectManager.GetTest((TestProjectId)projectId, session) : 
-                await _projectManager.Get((ProjectId)projectId, session);
-
-            string fileExtension;
-
-            switch (file.ContentType)
-            {
-                case "image/jpg":
-                case "image/jpeg":
-                case "image/pjpeg":
-                    fileExtension = ".jpg";
-                    break;
-                case "image/x-png":
-                case "image/png":
-                    fileExtension = ".png";
-                    break;
-                case "image/gif":
-                    fileExtension = ".gif";
-                    break;
-                default:
-                    throw new Exception("Incorrect file type");
-            }
-
-            var fileName = $"{Guid.NewGuid()}{fileExtension}";
-
-            // Does the user have access to the project?
-            if (!project.AdminIds.Contains(user.Id) && project.OwnerAdminId != user.Id)
-            {
-                throw new ApiException("Seems you are not an admin on this project.", (int)System.Net.HttpStatusCode.Unauthorized);
-            }
-            try
-            {
-                return Ok(await _fileManager.UploadFileAsync(file, fileName, $"devprojects/assets"));
-            }
-            catch (Exception e)
-            {
-
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-        }
+      _projectManager = projectManager;
+      _platformAdminUserManager = platformAdminUserManager;
+      _documentStore = documentStoreHolder;
+      _fileManager = fileManager;
+      _logger = logger;
     }
+
+    [HttpPost]
+    [Route("[action]/{projectNamespace:alpha}/{projectId}")]
+    public async Task<IActionResult> Save(IFormFile file, [FromRoute] string projectNamespace, [FromRoute] string projectId,
+        CancellationToken cancellationToken)
+    {
+      if (!string.Equals(file.ContentType, "image/jpg", StringComparison.OrdinalIgnoreCase) &&
+          !string.Equals(file.ContentType, "image/jpeg", StringComparison.OrdinalIgnoreCase) &&
+          !string.Equals(file.ContentType, "image/pjpeg", StringComparison.OrdinalIgnoreCase) &&
+          !string.Equals(file.ContentType, "image/gif", StringComparison.OrdinalIgnoreCase) &&
+          !string.Equals(file.ContentType, "image/x-png", StringComparison.OrdinalIgnoreCase) &&
+          !string.Equals(file.ContentType, "image/png", StringComparison.OrdinalIgnoreCase))
+      {
+        throw new Exception("Incorrect file type");
+      }
+
+      // Who's logged in?
+      using var session = _documentStore.OpenAsyncSession();
+      var user = await _platformAdminUserManager.GetByUniqueIdentifierAsync(User.Identity.Name, session);
+
+      projectId = $"{projectNamespace}/{projectId}";
+
+      var testMode = TestProjectId.IsValidIdentity(projectId) && !ProjectId.IsValidIdentity(projectId);
+
+      // Which project are we working on?
+      var project = testMode ?
+          await _projectManager.GetTest((TestProjectId)projectId, session) :
+          await _projectManager.Get((ProjectId)projectId, session);
+
+      string fileExtension;
+
+      switch (file.ContentType)
+      {
+        case "image/jpg":
+        case "image/jpeg":
+        case "image/pjpeg":
+          fileExtension = ".jpg";
+          break;
+        case "image/x-png":
+        case "image/png":
+          fileExtension = ".png";
+          break;
+        case "image/gif":
+          fileExtension = ".gif";
+          break;
+        default:
+          throw new Exception("Incorrect file type");
+      }
+
+      var fileName = $"{Guid.NewGuid()}{fileExtension}";
+
+      // Does the user have access to the project?
+      if (!project.AdminIds.Contains(user.Id) && project.OwnerAdminId != user.Id)
+      {
+        throw new ApiException("Seems you are not an admin on this project.", (int)System.Net.HttpStatusCode.Unauthorized);
+      }
+      try
+      {
+        return Ok(await _fileManager.UploadFileAsync(file, fileName, $"devprojects/assets"));
+      }
+      catch (Exception e)
+      {
+        _logger.LogError(e, "Unable to upload file.");
+        return StatusCode(StatusCodes.Status500InternalServerError);
+      }
+    }
+  }
 }
