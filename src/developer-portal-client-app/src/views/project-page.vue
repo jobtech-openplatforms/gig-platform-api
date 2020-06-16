348b7b7206a37f1ee7651f4df40d218187f0d52b
Johanna Selin <johanna@codeisking.se>
2020-06-16 09:36:47 +0200

    tightened view

--- src/developer-portal-client-app/src/views/project-page.vue
+++ src/developer-portal-client-app/src/views/project-page.vue
@@ -5,34 +5,40 @@
       // h1 {{currentProject.name}}
 
       current-step(:step="nextStep")
-      p Need help? #[router-link(to="/documentation") Read more in the full documentation]
+      p.small Need help? #[router-link(to="/documentation") Read more in the full documentation]
       ProjectEdit
 
         hr
         .integration-status
           strong Platform
           span.yes.ml-1 {{currentPlatform && (currentPlatform.published === true) ? ' Active' : ''}}
-          router-link.btn.btn-export.btn-outline.btn-small(to="/platform-settings") {{currentPlatform && currentPlatform.exportDataUri ? (currentPlatform.published ? 'Edit...' : 'Test...') : 'Add...'}}
-        doc-platform
+          .integration-status-control
+            .small
+              doc-platform
+            router-link.btn.btn-export.btn-outline.btn-small(to="/platform-settings") {{currentPlatform && currentPlatform.exportDataUri ? (currentPlatform.published ? 'Edit...' : 'Test...') : 'Add...'}}
+
 
         hr
         .integration-status
           strong Application
           span.yes.ml-1 {{currentApplication && currentApplication.authCallbackUrl ? 'Ready' : ''}}
-          router-link.btn.btn-import.btn-outline.btn-small(to="/application-settings" ) {{currentApplication && currentApplication.authCallbackUrl ? 'Edit...' : 'Add...'}}
-        doc-application
-      h2(style="margin-top:20px") Testing
+          .integration-status-control
+            .small
+              doc-application
+            router-link.btn.btn-import.btn-outline.btn-small(to="/application-settings" ) {{currentApplication && currentApplication.authCallbackUrl ? 'Edit...' : 'Add...'}}
+
+      h2.mt-4 Testing
       p.
         You can switch settings between live and a DEV mode.
         In DEV mode, there are no updates published through
         Open Platforms and is safe for development.
-      p(v-if="!devMode") You are currently in #[strong LIVE mode]. Try switching modes for local development.
-      p(v-if="devMode") You are currently in #[strong DEV mode]
 
-      .toggle-buttons.text-center
+      .toggle-buttons.text-center.my-4
         button.btn-big.toggle-button.btn-outline.btn-dev(@click="switchTestMode" v-bind:class="{ activestate: devMode}") DEV mode
         button.btn-big.toggle-button.btn-outline.btn-live(@click="switchTestMode" v-bind:class="{ activestate: !devMode}") Live mode
-      hr.my-4
+      em.small(v-if="!devMode") You are currently in #[strong LIVE mode]. Try switching modes for local development.
+      em.small(v-if="devMode") You are currently in #[strong DEV mode]
+
 
     div.home.project-page.center(v-else-if="currentProjects")
       .arrow
