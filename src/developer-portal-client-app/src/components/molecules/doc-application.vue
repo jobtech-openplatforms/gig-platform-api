10fd9d1792d5c37a8b40cfa1e56f8759a60dffb2
Johanna Selin <johanna@codeisking.se>
2020-06-16 09:33:04 +0200

    margin

--- src/developer-portal-client-app/src/components/molecules/doc-application.vue
+++ src/developer-portal-client-app/src/components/molecules/doc-application.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   //- div
       //- h3.color-import Application
-  .frame-wrapper
+  .frame-wrapper.mr-2
     p Let your users retrieve information from the platforms they have experience with to your application. Please note that approval is required.
       router-link.color-import(to="/application-documentation")  Read more
 </template>
